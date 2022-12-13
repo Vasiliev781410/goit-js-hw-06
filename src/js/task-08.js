1cee00283be4555b499dc60b702107b115eb547f
Vasiliev781410 <37668858+Vasiliev781410@users.noreply.github.com>
2022-12-13 11:03:08 +0200

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {elements: { email, password } } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    const message = "Please fill in all the fields!";
+    alert(message);
+    return;
+  }
+  const loginData = {
+    email: email.value,
+    password: password.value,
+  }
+  console.log(`Email: ${loginData.email}, Password: ${loginData.password}`);
+  event.currentTarget.reset();
+}
