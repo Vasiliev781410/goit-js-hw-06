4b4bf9e61aa1c01c0882fea577bcd99c721ea68a
Vasiliev781410 <37668858+Vasiliev781410@users.noreply.github.com>
2022-12-13 11:44:22 +0200

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const outputColor = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+
+const handleClick = (event) => {
+  const color = getRandomHexColor();
+  outputColor.textContent = color; 
+  document.body.style.backgroundColor = color;
+};
+
+button.addEventListener("click",handleClick);
