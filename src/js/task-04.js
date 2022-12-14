eb118ea64d923b3852792ab3619eafb1e171b98f
Vasiliev781410 <37668858+Vasiliev781410@users.noreply.github.com>
2022-12-14 11:27:31 +0200

    correct task 1, task 4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -6,26 +6,20 @@ const span = document.querySelector("#value");
 const buttonIncrement = span.nextElementSibling;
 
 let counterValue = 0;
-let counterValuePlus = 0;
 
 const handleClick = (event) => {    
     if (event.currentTarget === buttonDecrement){
         --counterValue;
-        updateDisplay(counterValue,event.currentTarget);        
+        updateDisplay(counterValue);        
     }else{        
-        ++counterValuePlus; 
-        updateDisplay(counterValuePlus,event.currentTarget);          
+        ++counterValue;             
     }
-      
+    updateDisplay(counterValue);    
 };
 
 buttonDecrement.addEventListener("click", handleClick);
 buttonIncrement.addEventListener("click", handleClick);
 
-function updateDisplay(value,elem){     
-    if (value > 0){  
-        elem.textContent = "+"+value; 
-        return; 
-    }
-    elem.textContent = value;
+function updateDisplay(value){     
+   span.textContent = value;
 }    
